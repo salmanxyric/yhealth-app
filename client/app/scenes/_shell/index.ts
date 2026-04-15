7a3eaa8ff442a24df8bdb62a168194703603c1ea
salmanxyric <salman@xyric.ai>
2026-04-15 14:07:55 +0500

    feat(scenes): barrel export for _shell

---
+++ client/app/scenes/_shell/index.ts
@@ -0,0 +1,6 @@
+export { Scene3D } from './Scene3D';
+export type { Scene3DKind } from './Scene3D';
+export { SceneSkeleton } from './SceneSkeleton';
+export { SceneShell } from './SceneShell';
+export { useSceneTimeline } from './useSceneTimeline';
+export type { SceneTimelineConfig, SceneTimelineResult } from './useSceneTimeline';
